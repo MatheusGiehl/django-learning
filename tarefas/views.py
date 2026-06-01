515b8a402021d1c81e8c39dbe4630ab43dcc2f25
Matheus Giehl <giehlmatheus96@gmail.com>
2026-05-31 22:48:06 -0300

    a3105

--- tarefas/views.py
+++ tarefas/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    frase = 'Olá Mundo!'
-    return HttpResponse(frase)      
+    return render(request,"tarefas/home.html")      
 
 def add(request):
     return HttpResponse("Adicione uma tarefa!")
